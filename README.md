9ede6e06132e6ebbb1e0c098dc9b6246673c5126
Senthil Nayagam <senthil@railsfactory.org>
2013-05-16 18:12:20 +0530

    TODO identified

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ pure-math.rb
 ============
 
 Math.c implemented in pure ruby
+
+identified the methods need to be implemented from math.c
+
+now need to port them
---
+++ math.rb
@@ -0,0 +1,65 @@
+
+
+class Math
+  
+ # Constants
+  PI = 3.14159_26535_89793_23846
+  E  = 2.71828_18284_59045_23536
+
+# TODO
+  def atan2(y, x)
+  end
+
+  def cos(x)
+  end
+
+  def sin(x)
+  end
+
+  def tan(x)
+  end
+
+  def atan(x)
+  end
+
+  def cosh(x)
+  end
+
+  def sinh(x)
+  end
+
+  def tanh(x)
+  end
+
+  def asinh(x)
+  end
+
+  def atanh(x)
+  end
+
+  def exp(x)
+  end
+
+  def frexp(x)
+  end
+
+  def ldexp(x, n)
+  end
+
+  def hypot(x, y)
+  end
+
+  def erf(x)
+  end
+
+  def erfc(x)
+  end
+
+  def gamma
+  end
+
+  def lgamma
+  end
+  
+end
+ 
